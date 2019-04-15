b097a42bc39ac85631d4d37ef94a90df26760b42
Nergaard <nergaard14@gmail.com>
2019-04-15 13:13:44 +0200

    m

--- index.php
+++ index.php
@@ -1,100 +1,16 @@
-<?php 
-	include 'frontpagephp.php';
- ?>
- <style>
- 	 <?php 
- 	 include 'indexstyle.css';
-  ?>
- </style>
+<?php
+	include_once('include/pdo_con_inc.php');
+	include_once('pages/head.php');
+	include_once('modules/header.php');
+?>
 
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Outgear</title>
-</head>
-<body>
-	<div class = header>
-		<a href="#index.php" class = "logo">Outgear</a>
-		<div class ="header-right">
-			<a class="home"  href="#index.php">Home</a>
-			<a class="articles"  href="articles.php">Articles</a>
-			<a class="about"  href="#about.php">About</a>
-		</div>
-	</div>
-	<div class="hero-image">
-	  <div class="hero-text">
-	    <h1>Welcome to Outgear</h1>
-	    <p>Articles, tips and tricks for adventure</p>
-	    <button>Articles</button>
-	  </div>
-	</div>
-	<div class="grid-banner1">
-	  <div class="grid-item">
-	  	<div class= "grid-photo-1" id = "photo-1">
-	  		<div class="grid-text-1">What to bring on a mountain hike
-	  		</div>
-	  	</div>
-	  </div>
-	</div>
-	<div class="grid-container">
-	  <div class="grid-item">
-	  	<div class= "grid-photo-1" id = "photo-1">
-	  		<div class="grid-text-1">What to bring on a mountain hike
-	  		</div>
-	  	</div>
-	  </div>
-	  <div class="grid-item">
-	  	<div class= "grid-photo-1" id ="photo-2">
-	  		<div class="grid-text-1">What to bring on a mountain hike
-	  		</div>
-	  	</div>
-	  </div>
-	  <div class="grid-item">
-	  	<div class= "grid-photo-1" id = "photo-3">
-	  		<div class="grid-text-1">What to bring on a mountain hike
-	  		</div>
-	  	</div>
-	  </div>
-	  <div class="grid-item">
-	  	<div class= "grid-photo-1" id = "photo-4">
-	  		<div class="grid-text-1">What to bring on a mountain hike
-	  		</div>
-	  	</div>
-	  </div>
-	  <div class="grid-item">
-	  	<div class= "grid-photo-1" id = "photo-5">
-	  		<div class="grid-text-1">What to bring on a mountain hike
-	  		</div>
-	  	</div>
-	  </div>
-	  <div class="grid-item">
-	  	<div class= "grid-photo-1" id = "photo-6">
-	  		<div class="grid-text-1">What to bring on a mountain hike
-	  		</div>
-	  	</div>
-	  </div>
-	  <div class="grid-item">
-	  	<div class= "grid-photo-1" id = "photo-7">
-	  		<div class="grid-text-1">What to bring on a mountain hike
-	  		</div>
-	  	</div>
-	  </div>
-	  <div class="grid-item">
-	  	<div class= "grid-photo-1" id = "photo-8">
-	  		<div class="grid-text-1">What to bring on a mountain hike
-	  		</div>
-	  	</div>
-	  </div>
-	  <div class="grid-item">
-	  	<div class= "grid-photo-1" id = "photo-9">
-	  		<div class="grid-text-1">What to bring on a mountain hike
-	  		</div>
-	  	</div>
-	  </div>	  	  	  	  	  	  
-	</div>
+<main class="main_wrapper">
+		<?php 
+			if (isset($_SESSION['u_id'])) {
+				include('modules/main_menu.php'); 
+			}
+		?>
+		<?php include('include/include.php'); ?>
+</main>
 
-<!--	<div class="footer">
-	  <p>Outgear</p>
-	</div>-->
-</body>
-</html>
+<?php include_once('pages/footer.php'); ?>
